0a1a471996f8276339ef5dd4f4a88121ee0a7d26
Eric Zhou <yiruiz2@linux-a2.ews.illinois.edu>
2020-11-27 23:45:24 -0600

    add function getDistace

--- utils.cpp
+++ utils.cpp
@@ -21,4 +21,23 @@ namespace utils {
     string strip(string& s) {
         return s.substr(1, s.size() - 2);
     }
+
+    double getDistance(double latitude1, double longtitude1, double latitude2, double longtitude2) {
+        /**
+         * Change to Radian  
+         * lat = latitude / 180 * pi 
+         * long = longt / 180 *pi
+         */
+        double latiR1 = latitude1 / 180 * M_PI;
+        double latiR2 = latitude2 / 180 * M_PI;
+        double longR1 = longtitude1 / 180 * M_PI;
+        double longR2 = longtitude2 / 180 * M_PI;
+
+        double latiDiff = latiR2 - latiR1;
+        double longDiff = longR2 - longR1;
+
+        double part1 = pow(sin(latiDiff / 2), 2) + pow(sin(longDiff / 2), 2) * cos(latiR1) * cos(latiR2); 
+        //6371 is the radian of earth in km
+        return 2 * asin(sqrt(part1)) * 6371;
+    }
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <math.h>
 
 using std::vector;
 using std::string;
@@ -26,4 +27,15 @@ namespace utils {
      * Strip all the quotation marks in the string
      */
     string strip(string &s);
+
+    /**
+     * Get the distance between two points on earth given their latitude and
+     * longtitude. 
+     * @param latitude1 latitude of the first point
+     * @param longtitude1 longtitude of the first point
+     * @param latitude2 latitude of the second point
+     * @param longtitude2 longtitude of the second point
+     * @returns distance between these two points
+     */
+    double getDistance(double latitude1, double longtitude1, double latitude2, double longtitude2);
 }
