2e72bd828cfe63de13772b79e2e988760d52128a
Joe Ye <joe.ye2010@qq.com>
2020-12-03 17:47:11 +0800

    modified airports.coo

--- airports.cpp
+++ airports.cpp
@@ -1,19 +1,17 @@
-#include "airports.h"
+#include "airlines.h"
 #include <fstream>  
 #include <unordered_map>
 #include "utils.h"
-#include <iostream>
 
 using std::cout;
 using std::endl;
 using std::ifstream;
 using std::unordered_map;
-using utils::split;
-using utils::strip;
+using utils::readEntry;
 
-void Airports::init() {
+void Airlines::init() {
     _map.clear();
-    ifstream infile("data/airports.dat");  
+    ifstream infile("data/airlines.dat");  
     string line;
     // handle error
     if (!infile.is_open()) {
@@ -22,27 +20,21 @@ void Airports::init() {
     }
     // loop through dat by lines
     while(getline(infile,line)) {  
-        vector<string> in = split(line, ','); 
+        vector<string> in = readEntry(line); 
         int id = std::stoi(in[0]);
-        //cout << id << endl;
-        string name = strip(in[1]);
-        string city = strip(in[2]);
-        string country = strip(in[3]);
-        string IATA = strip(in[4]);
-        string ICAO = strip(in[5]);
-        double latit = std::stod(in[6]);
-        double longit = std::stod(in[7]);
-        int alt = std::stoi(in[8]);
-        string timeZone = strip(in[9]);
-        string DST = strip(in[10]);
-        string tz = strip(in[11]);
-        AirportNode* data = new AirportNode(name, city, country, IATA, ICAO, latit, longit,
-            alt, timeZone, DST, tz);
+        string name = in[1];
+        string alias = in[2];
+        string IATA = in[3];
+        string ICAO = in[4];
+        string callsign = in[5];
+        string country = in[6];
+        bool active = in[7] == "Y";
+        AirlineNode* data = new AirlineNode(name, alias, IATA, ICAO, callsign, country, active);
         _map[id] = data;
     }
     infile.close();
 }
 
-Airports::AirportNode* Airports::getAirportByID(int id) {
+Airlines::AirlineNode* Airlines::getAirlineByID(int id) {
     return _map[id];
 }
