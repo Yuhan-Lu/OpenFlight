44a81260bfe1ba62d97dad2a177d5ab2a211f4af
Eric Zhou <yiruiz2@linux-a2.ews.illinois.edu>
2020-12-11 23:45:41 -0600

    main updated

--- main.cpp
+++ main.cpp
@@ -25,30 +25,32 @@ int main() {
     AirlineFlow airlineFlow(false); // load graphs, true for test routes, false for actual routes
     Airports* airports = airlineFlow.getAirportDataset();
     Airlines* airlines = airlineFlow.getAirlineDataset();
-    //AirlineFlow airlineFlow = new AirlineFlow(false);
-    //test getEdgeWeight
-    cout << airlineFlow.getRouteGraph()->getEdgeWeight(to_string(2965), to_string(2990)) << " km."  << endl;
 
 
-    //test getAirline
+    // test getEdgeWeight
+    cout << airlineFlow.getRouteGraph()->getEdgeWeight("2965", "2990") << " km."  << endl;
+
+
+    // test getAirline
     vector<int> airlinesRes = airlineFlow.getAirlineBetweenAirports("AER", "KZN");
     string airlinesReport = airlineFlow.getAirlineBetweenAirportsReport("AER", "KZN");
     cout << airlinesReport;
 
 
-    //test shortest_path
+    // test shortest_path
     Dijkstra dijkstra(airlineFlow);
     string dijkstraReport = dijkstra.getShortestPathReport(2965, 2922);
     cout << dijkstraReport;
     
   
-    //test page rank
+    // test PageRank
     Graph* g = airlineFlow.getRouteGraph();
     Pagerank* labelLoad = new Pagerank(airlineFlow, false);
     vector<pair<int, string>> pagerankRes = labelLoad->pageRank(0.85);
     string pageRankReport = labelLoad->getPageRankReport();
     cout << pageRankReport;
 
+    // test path drawer
     PathDrawer pathDrawer(airlineFlow.getAirportDataset());
     pair<int, vector<int>> path = dijkstra.shortestPath(2965, 2922);
     pathDrawer.addPath(path.second);
